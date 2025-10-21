41c5e550f3c816788e7f7459e4049a39b57389f4
David Rayner <david.rayner@gu.se>
2025-10-21 11:34:05 +0200

    change data file name to match instructions in prepdata

--- dashboard/shared.py
+++ dashboard/shared.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-data = pd.read_csv(app_dir / "data" / "researchdata_datasets.csv")
+data = pd.read_csv(app_dir / "data" / "metadata.csv")
 
 SourceRepositories=data['SourceRepository'].unique().tolist()
 Publishers = data['Publisher'].unique().tolist()
